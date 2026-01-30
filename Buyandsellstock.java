5af132a52f5673c1297f6ee4564a569d2fa143a5
Krishnagouda Patil <krishnagouda.patil@ellucian.com>
2026-01-30 22:06:40 +0530

    buy sell stocks

---
+++ Buyandsellstock.java
@@ -0,0 +1,18 @@
+public class Buyandsellstock {
+    public static void main(String[] args) {
+        int[] prices = {7, 1, 5, 3, 6, 4};
+        System.out.println(maxprofit(prices));
+    }
+    public static int maxprofit(int[] prices){
+        int min =prices[0];
+        int profit=0;
+        for(int i=0;i<prices.length;i++){
+            if(prices[i]<min){
+                min=prices[i];
+            }
+            profit = Math.max(profit, prices[i] - min);
+        }
+        return profit;
+    }
+}
+    
